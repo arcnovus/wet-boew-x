01f6e99e61b60035fb4002a6451f1326da5d1e7f
Bill Sourour <bill.sourour@arcnovus.com>
2021-09-17 08:58:18 -0400

    support doc title

--- packages/wet-boew-react/src/templates/SplashTemplate.tsx
+++ packages/wet-boew-react/src/templates/SplashTemplate.tsx
@@ -3,11 +3,13 @@ import {
   SplashTopProps,
   SplashTop,
   SplashContent,
+  useDocumentTitle,
 } from "../cdts";
 
 export type SplashTemplateProps = SplashContentProps & SplashTopProps;
 
 export function SplashTemplate(props: SplashTemplateProps) {
+  useDocumentTitle();
   return (
     <>
       <SplashTop {...props} />

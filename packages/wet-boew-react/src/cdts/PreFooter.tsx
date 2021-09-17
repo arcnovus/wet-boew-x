25ddb99232e4f25d21c0b0117142708b793f2cc8
Bill Sourour <bill.sourour@arcnovus.com>
2021-09-17 10:55:08 -0400

    remove unused

--- packages/wet-boew-react/src/cdts/PreFooter.tsx
+++ packages/wet-boew-react/src/cdts/PreFooter.tsx
@@ -1,13 +1,11 @@
 import { useState, useLayoutEffect } from "react";
 import { CdtsPreFooterParams, useCdts } from ".";
-import { useLanguage } from "../language";
 import { registerWetComponent } from "../wet";
 
 export type PreFooterProps = CdtsPreFooterParams;
 
 export function PreFooter(props: PreFooterProps) {
   const { wetBuilder, cdnEnv, cdnPath } = useCdts() ?? {};
-  const { currentLanguage } = useLanguage();
   const [__html, setHtml] = useState("");
   useLayoutEffect(() => {
     console.log("useLayoutEffect", "CdtsPreFooter.");

7bcddd82706049dc6687153ee565976390948f7d
Bill Sourour <bill.sourour@arcnovus.com>
2022-09-13 00:00:50 -0400

    add stories for all inputs

--- packages/wet-boew-react/stories/FormControl.stories.tsx
+++ packages/wet-boew-react/stories/FormControl.stories.tsx
@@ -1,6 +1,23 @@
 import { Story, Meta } from "@storybook/react";
-import { FormControl } from "../src";
-import { FormControlProps } from "@arcnovus/wet-boew-react/dist/forms/FormControl";
+import {
+  EmailInput,
+  FormControl,
+  PasswordInput,
+  TextInput,
+  TelInput,
+  NumberInput,
+  UrlInput,
+} from "../src";
+import type {
+  FormControlProps,
+  TextInputProps,
+  EmailInputProps,
+  PasswordInputProps,
+  NumberInputProps,
+  TelInputProps,
+  UrlInputProps,
+} from "../src";
+
 import React from "react";
 export default {
   title: "Components/Forms",
@@ -22,12 +39,76 @@ export default {
   },
 } as Meta;
 
-const Template = (args: FormControlProps) => <FormControl {...args} />;
-
-export const basicFormField: Story<FormControlProps> = Template.bind({});
-basicFormField.args = {
+const FormControlTemplate = (args: FormControlProps) => (
+  <FormControl {...args} />
+);
+export const formControl: Story<FormControlProps> = FormControlTemplate.bind(
+  {}
+);
+formControl.args = {
   label: "Label 1",
   id: "exampleInput1",
+  language: "en",
+  required: false,
+};
+
+const TextInputTemplate = (args: TextInputProps) => <TextInput {...args} />;
+export const textInput: Story<TextInputProps> = TextInputTemplate.bind({});
+textInput.args = {
+  label: "Text",
+  id: "exampleInput2",
+  language: "en",
+  required: false,
+};
+
+const EmailInputTemplate = (args: EmailInputProps) => <EmailInput {...args} />;
+export const emailInput: Story<EmailInputProps> = EmailInputTemplate.bind({});
+emailInput.args = {
+  label: "Email",
+  id: "exampleInput3",
+  language: "en",
+  required: false,
+};
+
+const PasswordInputTemplate = (args: PasswordInputProps) => (
+  <PasswordInput {...args} />
+);
+export const passwordInput: Story<PasswordInputProps> =
+  PasswordInputTemplate.bind({});
+passwordInput.args = {
+  label: "Password",
+  id: "exampleInput4",
+  language: "en",
+  required: false,
+};
+
+const NumberInputTemplate = (args: NumberInputProps) => (
+  <NumberInput {...args} />
+);
+export const numberInput: Story<NumberInputProps> = NumberInputTemplate.bind(
+  {}
+);
+numberInput.args = {
+  label: "Number",
+  id: "exampleInput5",
+  language: "en",
+  required: false,
+};
+
+const TelInputTemplate = (args: TelInputProps) => <TelInput {...args} />;
+export const telInput: Story<TelInputProps> = TelInputTemplate.bind({});
+telInput.args = {
+  label: "Tel",
+  id: "exampleInput6",
+  language: "en",
+  required: false,
+};
+
+const UrlInputTemplate = (args: UrlInputProps) => <UrlInput {...args} />;
+export const urlInput: Story<UrlInputProps> = UrlInputTemplate.bind({});
+urlInput.args = {
+  label: "URL",
+  id: "exampleInput7",
   language: "en",
   required: false,
 };
